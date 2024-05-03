11d20da9c915a797c11da2ab29e9c9980a9aadfd
Santiago Silvestrini <santiago.silvestrini@followoz.com>
2024-05-03 15:24:02 -0300

    corrijo funcion sumar

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -2,7 +2,7 @@ library(testthat)
 
 source(here::here('code/functions.R'))
 
-test_that("Tests para mi function",
+test_that("Tests para mi function sumar",
           {
             expect_equal( sumar(5,1), 6 )
             expect_equal( sumar(3,5), 8 )
